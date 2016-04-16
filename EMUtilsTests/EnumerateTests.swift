6930d32f7cb1dad3e60808f8217e2a053c7fccb1
Ewan Mellor <commits@ewanmellor.org>
2016-04-16 12:07:05 -0700

    Added some tests.

--- EMUtilsTests/EnumerateTests.swift
+++ EMUtilsTests/EnumerateTests.swift
@@ -39,4 +39,56 @@ class EnumerateTests: EMTestCaseBase {
 
         XCTAssertEqual(result, expected)
     }
+
+    func testEnumerateWithResult() {
+        let a1 = [1, 2, 3, 4]
+        let a2 = [4, 5, 6, 7]
+        var result = [Int]()
+        let expected = [5, 7, 9, 11]
+
+        let returnedVal = Enumerate.pairwiseOver(a1, and: a2) { (o1, o2) -> (Bool, String?) in
+            result.append(o1 + o2)
+            if o1 == 4 {
+                return (true, "Spiffing")
+            }
+            else {
+                return (false, nil)
+            }
+        }
+
+        XCTAssertEqual(result, expected)
+        XCTAssertEqual(returnedVal, "Spiffing")
+    }
+
+
+    func testEnumerateWithResultDifferentLengthsReachingEnd() {
+        let a1 = [1, 2, 3]
+        let a2 = [4, 5, 6, 7]
+        var result = [Int]()
+        let expected = [5, 7, 9]
+
+        let returnedVal = Enumerate.pairwiseOver(a1, and: a2) { (o1, o2) -> (Bool, String?) in
+            result.append(o1 + o2)
+            return (false, nil)
+        }
+
+        XCTAssertEqual(result, expected)
+        XCTAssertNil(returnedVal)
+    }
+
+
+    func testEnumerateWithResultDifferentLengthsNotReachingEnd() {
+        let a1 = [1, 2, 3]
+        let a2 = [4, 5, 6, 7]
+        var result = [Int]()
+        let expected = [5, 7]
+
+        let returnedVal = Enumerate.pairwiseOver(a1, and: a2) { (o1, o2) -> (Bool, Any?) in
+            result.append(o1 + o2)
+            return (o1 == 2, nil)
+        }
+
+        XCTAssertEqual(result, expected)
+        XCTAssertNil(returnedVal)
+    }
 }
