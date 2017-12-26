89d01025392fbe1a2f7921ad1b81f808c7f1daca
Ewan Mellor <commits@ewanmellor.org>
2017-12-25 21:53:48 -0800

    Make WeakWrapper final.
    Add WeakWrapperHashable, which is the same as WeakWrapper but wrapping
    Hashable objects and implementing == and hashValue to suit.

--- EMUtils/WeakWrapper.swift
+++ EMUtils/WeakWrapper.swift
@@ -9,9 +9,34 @@
 import Foundation
 
 
-public class WeakWrapper<T: AnyObject> {
+public final class WeakWrapper<T: AnyObject> {
     public weak var value : T?
     public init(_ value: T) {
         self.value = value
+    }
+}
+
+
+public final class WeakWrapperHashable<T>: Hashable where T: AnyObject, T: Hashable {
+    public static func ==(lhs: WeakWrapperHashable<T>, rhs: WeakWrapperHashable<T>) -> Bool {
+        if lhs === rhs {
+            return true
+        }
+        guard let l = lhs.value, let r = rhs.value else {
+            return false
+        }
+        return l == r
+    }
+
+    public weak var value : T?
+    private let _hash: Int
+
+    public init(_ value: T) {
+        self.value = value
+        _hash = value.hashValue
+    }
+
+    public var hashValue: Int {
+        return _hash
     }
 }
