f2aaee1c81556ac786a568558e98c0eea9154bbc
Ewan Mellor <commits@ewanmellor.org>
2020-01-11 14:16:30 -0800

    Added CharacterSet.slashOnly.

---
+++ EMUtils/CharacterSet+EMUtils.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterSet+EMUtils.swift
+//  EMUtils
+//
+//  Created by Ewan Mellor on 1/11/20.
+//  Copyright © 2020 Ewan Mellor. All rights reserved.
+//
+
+import Foundation
+
+
+public extension CharacterSet {
+    static let slashOnly = CharacterSet(["/"])
+}
